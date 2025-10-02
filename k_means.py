7f455e4c034926b96d27fdc1ff35cd8baaa62783
Quinn Kelley <qkelley0726@gmail.com>
2025-10-02 11:51:50 -0400

    No Comments yet, but added metrics systems to code

--- k_means.py
+++ k_means.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple
+import Loading
     
 def euclidean_distance(point1: np.ndarray, point2: np.ndarray) -> float:
     return np.sqrt(np.sum((point1 - point2) ** 2))
@@ -150,6 +151,7 @@ def train_hyperparameters(max_k: int, max_threshold: int, iterations: int, data:
     threshold_values = list()
     k_variances = list()
     t_variances = list()
+    c_best=[5,5.0,0,0,1,0]
     
     # Train k value with fixed threshold
     for k in range(1, max_k):
@@ -157,6 +159,8 @@ def train_hyperparameters(max_k: int, max_threshold: int, iterations: int, data:
         clusters, centroids, _ = fit(k, data, centroids, iterations, 5.0)
         k_values.append(k)
         k_variances.append(intercluster_variance(data, clusters, centroids))
+        acc, tpr, fpr, f1_s = metrics(k, 5.0, False)
+        c_best = store_best(k, 5.0, acc, tpr, fpr, f1_s, c_best)
 
     # Train distance threshold with fixed k value
     for t in range(1, max_threshold):
@@ -164,16 +168,63 @@ def train_hyperparameters(max_k: int, max_threshold: int, iterations: int, data:
         clusters, centroids, _ = fit(5, data, centroids, iterations, t)
         threshold_values.append(t)
         t_variances.append(intercluster_variance(data, clusters, centroids))
+        acc, tpr, fpr, f1_s = metrics(5, t, False)
+        c_best = store_best(5, t, acc, tpr, fpr, f1_s, c_best)
 
     plot_hyperparameters(k_values, threshold_values, k_variances, t_variances)
+    print("=== Best Hyperparameters ===")
+    print(f"K: {c_best[0]}, Threshold: {c_best[1]}")
+    print(f"Accuracy: {c_best[2]:.4f}, \nTPR: {c_best[3]:.4f}, \nFPR: {c_best[4]:.4f}, \nF1-Score: {c_best[5]:.4f}")
 
-def test():
-    import Loading
-    
+def test_hparam():
     max_k = 19
     max_threshold = 7
     iterations = 50
     data = Loading.load_data(0)[1]
     train_hyperparameters(max_k, max_threshold, iterations, data)
 
-test()
+def metrics(k_val=5, thresh=5.0, p_print=True):
+
+    t_data, a_data, n_data = Loading.load_data()
+    data_r = np.vstack([n_data, a_data])
+    y_true = np.concatenate([np.zeros(len(n_data), dtype=int),np.ones(len(a_data), dtype=int)])
+
+
+    centroids = initialize_centroids(k_val, data=t_data)
+    clusters = assign_clusters(data_r, centroids)
+    anom = detect_anomalies(data_r, centroids, clusters, thresh)
+    y_pred = anom.astype(int)
+
+    TP = np.sum((y_true == 1) & (y_pred == 1))
+    TN = np.sum((y_true == 0) & (y_pred == 0))
+    FP = np.sum((y_true == 0) & (y_pred == 1))
+    FN = np.sum((y_true == 1) & (y_pred == 0))
+
+    acc = Loading.accuracy(TP, TN, FP, FN)
+    tpr = Loading.tpr(TP, FN)
+    fpr = Loading.fpr(FP, TN)
+    f1_s = Loading.f1_score(TP, FP, FN)
+
+    if p_print:
+        print("=== Evaluation Metrics K="+ str(k_val) + " & Threshold=" + str(thresh) + " ===")
+        print(f"Accuracy : {acc:.4f}")
+        print(f"TPR (Recall) : {tpr:.4f}")
+        print(f"FPR : {fpr:.4f}")
+        print(f"F1-Score : {f1_s:.4f}")
+
+    return acc, tpr, fpr, f1_s
+
+def store_best(k_val, thresh, acc, tpr, fpr, f1_s, c_best=[5,5.0,0,0,1,0]):
+    if acc > c_best[2]:
+        c_best[0] = k_val
+        c_best[1] = thresh
+        c_best[2] = acc
+        c_best[3] = tpr
+        c_best[4] = fpr
+        c_best[5] = f1_s
+    return c_best
+
+
+if __name__ == "__main__":
+    test_hparam()
+
